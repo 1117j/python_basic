b6f10739c4bb4a5b9cf31a79d90db4d567f55520
Hyejin Lee <63032376+1117j@users.noreply.github.com>
2024-02-18 18:22:27 +0900

    Initial Commit

---
+++ calculator.py
@@ -0,0 +1,26 @@
+print(4+5) 
+#Addition 
+print(9*5)
+#Multiplication 
+print(-1/4)
+# Subtraction 
+print(1/3)
+# Division 
+print(((2050/4)-32)/9)
+# Compound Operation 
+print(2**2)
+# Exponentiation 
+
+
+"""
+x + y            Addition + operator returns the sum of x plus y
+x - y             Subtraction - operator returns the difference of x minus y
+x * y            Multiplication * operator returns the product of x times y
+x / y             Division / operator returns the quotient of x divided by y
+x**e            Exponent ** operator returns the result of raising x to the power of e 
+x**2            Square expression returns x squared
+x**3            Cube expression returns x cubed
+x**(1/2) 제곱근   Square root (½) or (0.5) fractional exponent operator returns the square root of x
+x // y  나누기 정수만 반환  Floor division operator returns the integer part of the integer division of x by y
+x % y  나머지 Modulo operator returns the remainder part of the integer division of x by y
+"""
---
+++ hello_world.py
@@ -0,0 +1,5 @@
+print("hello world!")
+
+# keyword : for if while else etc. 
+# I can use "" or '' for  
+# in C -> 3 lines  
